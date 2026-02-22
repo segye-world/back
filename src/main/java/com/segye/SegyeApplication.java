c9bf1d498902e18a07f35759189928a5cacf9c8f
chaewon5227 <chaewon5228@naver,com>
2026-02-22 12:26:10 +0900

    연결 문제 확인

--- src/main/java/com/segye/SegyeApplication.java
+++ src/main/java/com/segye/SegyeApplication.java
@@ -1,8 +1,11 @@
 package com.segye;
 
+import com.segye.auth.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(JwtProperties.class)
 @SpringBootApplication
 public class SegyeApplication {
 
--- src/main/java/com/segye/auth/JwtProperties.java
+++ src/main/java/com/segye/auth/JwtProperties.java
@@ -3,6 +3,7 @@ package com.segye.auth;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
